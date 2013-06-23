d8e2b9dcc916b5586b8e3facd683cc3077b3ba49
Tomek Wałkuski <ja@jestem.tw>
2013-06-23 22:31:05 +0200

    Update Clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject cotd "0.1.0"
+(defproject cotd "0.1.1"
   :description  "Clojure of the day"
   :url          "https://github.com/tomekw/cotd"
   :license      { :name "Eclipse Public License"
                   :url  "http://www.eclipse.org/legal/epl-v10.html" }
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main         cotd.core)
